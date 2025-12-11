4d1724d6e44d8aa4001fb115c68d638fbd0cac03
MakhloufiAdnan <makhloufiadnan@gmail.com>
2025-12-11 16:37:58 +0100

    refactor: enhance Jest configuration, improve test coverage scripts, and update service tests for better clarity and functionality

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,32 @@
 module.exports = {
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  testMatch: ["**/*.spec.ts"],
   moduleNameMapper: {
-    '@core/(.*)': '<rootDir>/src/app/core/$1',
+    "@core/(.*)": "<rootDir>/src/app/core/$1",
   },
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  bail: false,
-  verbose: false,
-  collectCoverage: false,
-  coverageDirectory: './coverage/jest',
-  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-  coveragePathIgnorePatterns: ['<rootDir>/node_modules/'],
+  roots: ["<rootDir>"],
+  modulePaths: ["<rootDir>"],
+  moduleDirectories: ["node_modules"],
+
+  // --- Couverture ---
+  collectCoverage: true,
+  coverageDirectory: "./coverage/jest",
+  coverageReporters: ["html", "lcov", "text-summary"],
+  collectCoverageFrom: [
+    "src/app/**/*.ts",
+    "!src/main.ts",
+    "!src/polyfills.ts",
+    "!src/environments/**",
+    "!src/app/**/*.spec.ts",
+  ],
+  coveragePathIgnorePatterns: ["<rootDir>/node_modules/"],
   coverageThreshold: {
     global: {
-      statements: 80
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
     },
   },
-  roots: [
-    "<rootDir>"
-  ],
-  modulePaths: [
-    "<rootDir>"
-  ],
-  moduleDirectories: [
-    "node_modules"
-  ],
 };
